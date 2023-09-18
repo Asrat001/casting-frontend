cc3f026a891c98729317029156d7ff4610d1f37e
assrat007 <asratadane169@gmail.com>
2023-09-19 01:39:01 +0300

    fixing padding

--- src/componets/Hero.jsx
+++ src/componets/Hero.jsx
@@ -5,7 +5,7 @@ import hero from "../assets/hero.png"
 const Hero = () => {
   return (
     <section className=' grid grid-cols-1 sm:grid-cols-2 p-8 mt-[100px] px-[60px]'>
-    <main className='mt-[60px] '>
+    <main className='mt-[10px] '>
      <h1 className=' text-white font-bold text-[70px]'>we bring the Best talents </h1>
      <p className=' text-[20px]  text-white'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore optio, animi nam odit provident atque.</p>
      <div className=' flex items-center gap-x-3 mt-6'>
