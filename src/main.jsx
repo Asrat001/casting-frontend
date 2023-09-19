55a04b0d34d20c1e478cdb3c5bbdeb21a4ead474
assrat007 <asratadane169@gmail.com>
2023-09-19 21:48:32 +0300

    routing and folder stracture done

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,11 @@
 
-import  Home from "./pages/Home"
-import LoginPage from "./pages/Login"
+import App from "./App";
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import "./index.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home/>,
-  },
-  {
-    path: "/login",
-    element: <LoginPage/>,
-  },
- 
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App/>
   </React.StrictMode>
 );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,12 +8,11 @@ import CastCard from "../componets/CastCard";
 function Home() {
 
   return (
-    <div>
-      <div className=" bg-hero-bg  bg-cover bg-no-repeat  h-screen">
-        <Navbar />
-        <Hero />
-      </div>
-      <div className="px-[80px] mt-[50px] grid grid-cols-4 place-items-center justify-items-center">
+    
+    <section className="">
+     <Hero/>
+     <main className=" bg-[#243046] p-4">
+      <div className=" mt-[50px] grid grid-cols-3 place-items-center justify-items-center ">
         <p className="text-[32px] font-bold text-white">cast catagory</p>
         <div className=" rounded-full border-green-400 border-[3px] pl-3  w-full flex justify-center items-center">
           <BsSearch className="w-8 h-8 text-white" />
@@ -23,27 +22,32 @@ function Home() {
             className=" border-none outline-none text-white   focus:border-sky-500 bg-transparent rounded-r-full p-2  w-full border-green-400"
           />
         </div>
-        <div>
-          <select className=" outline-none p-2 rounded-lg bg-green-400 border-[4px] border-white">
-            <option disabled>skin color</option>
+        <div className=" bg-slate-900 space-x-2 px-3 py-2 rounded-xl">
+          <select className=" outline-none p-2  text-white  bg-slate-800 ">
+            <option >skin color</option>
             <option className="text-green-400">black</option>
             <option>brown</option>
             <option>white</option>
           </select>
-        </div>
-        <div>
-          <select className=" outline-none p-2 rounded-lg bg-green-400 border-[4px] border-white">
+          <select className=" outline-none p-2 text-white  bg-slate-800 ">
+            <option disabled>age</option>
+            <option className="">16-25</option>
+            <option>25-40</option>
+            <option>40-60</option>
+          </select>
+          <select className=" outline-none p-2 text-white  bg-slate-800 ">
             <option disabled>sex</option>
             <option>femeal</option>
             <option>men</option>
           </select>
         </div>
       </div>
-      <div className="mt-20">
+      <div className="mt-24">
       <CastCard/>
       </div>
-      
-    </div>
+      </main>
+      </section>
+    
   );
 }
 
