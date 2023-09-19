734d68d81c5748ca9611acca94bda22f0c076a0a
assrat007 <asratadane169@gmail.com>
2023-09-19 15:06:46 +0300

    react router

--- src/componets/CastCard.jsx
+++ src/componets/CastCard.jsx
@@ -4,9 +4,12 @@ const CastCard = () => {
   return (
     <div className='grid grid-cols-4 place-content-center justify-items-center gap-4'>
 {
-    Data.map((key , data)=>{
+    Data.map((data,key)=>{
         return(
             <div key={key} className='w-[300px] h-[400px] bg-transparent  border-[4px] border-green-400 rounded-xl '>
+                <img src={data.img} className=' h-1/2  object-cover w-full '/>
+                <p className='text-white'>Name:{data.name}</p>
+                <p className='text-white'>Catagory:{data.role}</p>
 
             </div>
         )
