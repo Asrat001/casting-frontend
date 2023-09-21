4260bf9754d9aeb7d665685e295047a1cb2fb192
assrat007 <asratadane169@gmail.com>
2023-09-21 10:53:32 +0300

    perofile edited

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -48,6 +48,28 @@ const Profile = () => {
          <UploadPics/>
          <form className="mt-6">
            <main className='grid grid-cols-1 sm:grid-cols-2 gap-4 '>
+           <div>
+              
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                About your self
+              </label>
+             
+                <div className="mt-1">
+                <textarea
+                  maxLength={250}
+                  name="language"
+                  autoComplete="name"
+                  placeholder="i'm pacienate actor and film maker...."
+                  required
+                  onChange={(e) => setLanguage(e.target.value)}
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                />
+              </div>
+            
+            </div>
          <div>
               <label
                 htmlFor="email"
@@ -67,6 +89,7 @@ const Profile = () => {
               </div>
             </div>
             <div>
+              
               <label
                 htmlFor="email"
                 className="block text-sm font-medium text-gray-700"
@@ -249,7 +272,7 @@ const Profile = () => {
                   type='text'
                   name="text"
                   autoComplete="name"
-                  placeholder='ex: Music video, Movie , commercial ad'
+                  placeholder='ex: Music video, Movie , promrotion'
                   required
                   onChange={(e) => setLanguage(e.target.value)}
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
