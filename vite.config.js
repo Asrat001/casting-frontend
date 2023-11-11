a8aee976c843660f610705a8b8c4dbb529e056f3
assrat007 <asratadane169@gmail.com>
2023-11-11 17:27:00 +0300

    debloy build

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    jsxFactory: 'React.createElement',
-    jsxInject: `import * as React from 'react'`,
-    jsx: 'react',
-    // Add the following line:
-    throwIfNamespace: false,
-  },
+
 })
 
