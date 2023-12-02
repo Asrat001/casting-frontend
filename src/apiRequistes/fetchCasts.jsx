6e87328c26100bb71b20904bbf4fcbbfa2c4107d
assrat007 <asratadane169@gmail.com>
2023-12-02 15:36:48 +0300

    server

--- src/apiRequistes/fetchCasts.jsx
+++ src/apiRequistes/fetchCasts.jsx
@@ -1,15 +1,16 @@
 import axios from "axios"
+import { server } from "../server"
 
 
  const fetchCasts =async ( filter)=>{
     console.log(filter)
-    return await axios.get(`http://localhost:8000/api/user/alluser?search=${filter.search}&limit=6&page=${filter.page}&sex=${filter.gender ?filter.gender:''}&minAge=0&maxAge=30`)
+    return await axios.get(`${server}/api/user/alluser?search=${filter.search}&limit=6&page=${filter.page}&sex=${filter.gender ?filter.gender:''}&minAge=0&maxAge=30`)
    }
 const fetchCountedata = async()=>{
-    return await axios.get(`http://localhost:8000/api/user/countusers`,{withCredentials:true})
+    return await axios.get(`${server}/api/user/countusers`,{withCredentials:true})
 }
 const fetchCountorders = async ()=>{
-    return await axios.get(`http://localhost:8000/api/order/countorders`,{withCredentials:true})
+    return await axios.get(`${server}/api/order/countorders`,{withCredentials:true})
 }
 
 export{fetchCasts,fetchCountedata,fetchCountorders}
